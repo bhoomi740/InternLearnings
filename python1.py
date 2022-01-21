43c777b9053d10cf9038930a27c4f3ab7ba5be85
bhoomi740-Git <bhoomikas909@gmail.com>
2022-01-21 16:42:44 +0530

    python assignment

---
+++ python1.py
@@ -0,0 +1,7 @@
+import pandas as pd
+out = pd.read_csv(r'C:\Users\bhoomika\Downloads\output_file.csv')
+recipe = pd.read_excel(r'C:\Users\bhoomika\Downloads\recipe.xlsx')
+# converting to dictionary
+out.dropna(inplace=True)
+out_dict=out.to_dict()
+print(out_dict)
