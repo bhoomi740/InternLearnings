21712a669baf0e711f1133b41eb92030eca0aa20
bhoomi740-Git <bhoomikas909@gmail.com>
2022-01-27 01:14:18 +0530

    Program on pandas - to sortv

---
+++ py2.py
@@ -0,0 +1,7 @@
+# Pandas program to sort a given Series
+import pandas as pd
+s = pd.Series(['100', '200', 'python', '300.12', '400'])
+print("Original Data Series:")
+print(s)
+new_s = pd.Series(s).sort_values()
+print(new_s)
