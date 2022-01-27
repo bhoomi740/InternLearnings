de8d3d87baa02c531cfdb75a71a9e90414f00cd3
bhoomi740-Git <bhoomikas909@gmail.com>
2022-01-27 09:05:02 +0530

    pandas program to remove white spaces

---
+++ py6.py
@@ -0,0 +1,11 @@
+# Pandas program to remove whitespaces
+import pandas as pd
+color1 = pd.Index([' Green', 'Black ', ' Red ', 'White', ' Pink '])
+print("Original series:")
+print(color1)
+print("\nRemove whitespace")
+print(color1.str.strip())
+print("\nRemove left sided whitespace")
+print(color1.str.lstrip())
+print("\nRemove Right sided whitespace")
+print(color1.str.rstrip())
