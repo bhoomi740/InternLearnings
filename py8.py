8bd5bd265b2029ec6ed076423fa043ab759e2c5f
bhoomi740-Git <bhoomikas909@gmail.com>
2022-01-27 09:52:59 +0530

    pandas program to split a string of column

---
+++ py8.py
@@ -0,0 +1,12 @@
+#Pandas program to split a string of a column of a given DataFrame into multiple columns.
+import pandas as pd
+df = pd.DataFrame({
+    'name': ['Alberto  Franco','Gino Ann Mcneill','Ryan  Parkes', 'Eesha Artur Hinton', 'Syed  Wharton'],
+    'date_of_birth ': ['17/05/2002','16/02/1999','25/09/1998','11/05/2002','15/09/1997'],
+    'age': [18.5, 21.2, 22.5, 22, 23]
+})
+print("Original DataFrame:")
+print(df)
+df[["first", "middle", "last"]] = df["name"].str.split(" ", expand = True)
+print("\nNew DataFrame:")
+print(df)
